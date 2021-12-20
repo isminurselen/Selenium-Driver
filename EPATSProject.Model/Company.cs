bc26dfc6b36acedacfc6f2aa6782a78db18701f1
isminurselen <50100904+isminurselen@users.noreply.github.com>
2021-12-20 10:40:00 +0300

    Add files via upload

---
+++ EPATSProject.Model/Company.cs
@@ -0,0 +1,18 @@
+﻿namespace EPATSProject.Model
+{
+    public class Company
+    {
+        public string Type { get; set; }
+        public string TaxNumber { get; set; }
+        public string TaxOffice { get; set; }
+        public string Nationality { get; set; }
+
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
